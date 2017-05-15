5bbcdb661bbdb00e0fafa5365da1349342514eb2
shinayoshi <ryota+git@shinayoshi.net>
2017-05-15 23:31:08 +0900

    add jsoup sample

--- src/net/shinayoshi/aoh/Main.java
+++ src/net/shinayoshi/aoh/Main.java
@@ -1,8 +1,19 @@
 package net.shinayoshi.aoh;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello IntelliJ!");
+    private static String url = "http://www.shinayoshi.net";
+
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect(url).get();
+        Elements newsHeadlines = doc.select("h1");
+        for (Element element : newsHeadlines) {
+            System.out.println(element.text());
+        }
     }
 }
